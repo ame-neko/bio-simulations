c8d077183d7d09fc67706ca5c7c989b29116f78a
miyamoto <59051990+pixel-png@users.noreply.github.com>
2025-08-23 22:51:53 +0900

    Remove www directory

--- predator_prey/www/pkg/predator_prey_bg.wasm.d.ts
+++
@@ -1,6 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-export const memory: WebAssembly.Memory;
-export const simulate: (a: number, b: number, c: number, d: number, e: number, f: number, g: number, h: number) => any;
-export const __wbindgen_export_0: WebAssembly.Table;
-export const __wbindgen_start: () => void;
